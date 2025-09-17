95aa49bdd73a0aadec1c249bff96531ba2af237c
cyriannechabert <cyrianne.chabert@protonmail.com>
2025-09-16 23:54:29 -0400

    updated photo

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import avatar from '../assets/images/avatar.jpg';
 import '../assets/styles/Main.scss';
 
 function Main() {
@@ -9,7 +10,7 @@ function Main() {
     <div className="container">
       <div className="about-section">
         <div className="image-wrapper">
-          <img src="https://my-aws-assets.s3.us-west-2.amazonaws.com/portfolio-img/avatar_circle.jpeg" alt="Avatar" />
+          <img src={avatar} alt="Avatar" className="avatar" />
         </div>
         <div className="content">
           <div className="social_icons">
@@ -17,7 +18,8 @@ function Main() {
             <a href="https://www.linkedin.com/in/cyriannechabert" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
           </div>
           <h1>Cyrianne Chabert</h1>
-          <p>Mechanical Engineering Student at Columbia University - Mines ParisTech</p>
+          <p>Mechanical Engineering Student<p>
+          </p>Columbia University - Mines ParisTech</p>
 
           <div className="mobile_social_icons">
             <a href="https://github.com/cyriannechabert" target="_blank" rel="noreferrer"><GitHubIcon/></a>
